10098b9ce86d700b484244d2e1f285bebc102d69
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-11-25 16:49:38 -0300

    refactor: Remove commented-out code and clean up App component

--- App copy.js
+++ App copy.js
@@ -1,47 +1,47 @@
-// import React, { useEffect, useState } from "react";
-// import { Text, View, StyleSheet, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 
-// import apiService from "./service/apiService";
+import apiService from "./service/apiService";
 
-// const App = () => {
-//   const [perfis, setPerfis] = useState([]);
+const App = () => {
+  const [perfis, setPerfis] = useState([]);
 
-//   useEffect(() => {
-//     const perfil = async () => {
-//       try {
-//         const listaSimples = await apiService.getPerfisSimples();
-//         setPerfis(listaSimples);
-//       } catch (error) {
-//         console.error("Erro ao buscar perfis:", error);
-//       }
-//     };
+  useEffect(() => {
+    const perfil = async () => {
+      try {
+        const listaSimples = await apiService.getPerfisSimples();
+        setPerfis(listaSimples);
+      } catch (error) {
+        console.error("Erro ao buscar perfis:", error);
+      }
+    };
 
-//     perfil();
-//   }, []);
+    perfil();
+  }, []);
 
-//   return (
-//     <View style={styles.container}>
-//       <Text>Lista de Perfis (via apiService)</Text>
-//       <FlatList
-//         data={perfis}
-//         keyExtractor={(item) => item.email}
-//         renderItem={({ item }) => (
-//           <View>
-//             <Text>Nome: {item.nome}</Text>
-//             <Text>Email: {item.email}</Text>
-//           </View>
-//         )}
-//       />
-//     </View>
-//   );
-// };
+  return (
+    <View style={styles.container}>
+      <Text>Lista de Perfis (via apiService)</Text>
+      <FlatList
+        data={perfis}
+        keyExtractor={(item) => item.email}
+        renderItem={({ item }) => (
+          <View>
+            <Text>Nome: {item.nome}</Text>
+            <Text>Email: {item.email}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
-// export default App;
+export default App;
