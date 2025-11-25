ab7a1bbbd8584eb979c684d4e1e863e7dfe952a4
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-11-25 16:28:57 -0300

    feat: Add main application logic and API service integration
    - Implemented main application structure in App component with navigation and state management.
    - Added Axios as a dependency for API requests.
    - Created apiService.js for handling API interactions, including fetching profiles, historical data, and operations.
    - Integrated UI components for displaying WiFi status, historical occurrences, and emergency procedures.
    - Added functionality for refreshing data and handling emergency calls.

---
+++ service/apiService.js
@@ -0,0 +1,86 @@
+import axios from 'axios';
+
+// URL base da sua API
+const BASE_URL = 'http://aninhakatsumata.somee.com/api';
+
+// 1. Cria a instância do Axios com a URL base
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  timeout: 10000,
+});
+
+/**
+ * Função para buscar os perfis.
+ * Faz a requisição e já retorna a lista filtrada (nome e email).
+ */
+const getPerfisSimples = async () => {
+  try {
+    // A chamada é feita apenas para o path '/perfis', 
+    // pois a BASE_URL já está configurada na instância 'api'
+    const response = await api.get('/perfis');
+    
+    const listaCompleta = response.data;
+    
+    // Filtra/Mapeia a lista para retornar APENAS nome e email
+    const listaSimples = listaCompleta.map(perfil => ({
+      nome: perfil.nome,
+      email: perfil.email,
+    }));
+    
+    return listaSimples; // Retorna a lista já limpa
+    
+  } catch (error) {
+    // Re-lança o erro para ser tratado no componente (tela)
+    throw error;
+  }
+};
+
+
+const getHistoricos = async () => {
+  try {
+    const response = await api.get('/historicos');
+    
+    const listaCompleta = response.data;
+    
+    const listaSimples = listaCompleta.map(historico => ({
+      status: historico.status,
+      dataFim: historico.dataFim,
+    }));
+    
+    return listaSimples;
+    
+  } catch (error) {
+
+    throw error;
+  }
+};
+
+const getOperacoes = async () => {
+  try {
+    const response = await api.get('/operacoes');
+
+    const listaCompleta = response.data;
+
+    const listaSimples = listaCompleta.map(operacao => ({
+      operacao: operacao.status,
+    }));
+    
+    return listaSimples;
+    
+  } catch (error) {
+
+    throw error;
+  }
+};
+
+
+// Exporta as funções que o componente usará
+const apiService = {
+  getPerfisSimples, getHistorico, getOperacoes,
+  // Você pode adicionar outras funções como getHistorico, postLogin, etc.
+};
+
+export default apiService;
